827d689ef14ffcc113c0a6f19875576d641cb5cb
Ibrahim Ozan Genç <ibrahimozan.genc@bilgeadamboost.com>
2023-05-18 12:23:00 +0300

    Datalar yüklendi.

---
+++ KafeKonsol.Data/SiparisDetay.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeKonsol.Data
+{
+    public class SiparisDetay
+    {
+        public string UrunAd { get; set; }
+        public decimal BirimFiyat { get; set; }
+        public int Adet { get; set; }
+        public string TutarTL { get; }
+
+        public void Tutar(decimal tutar)
+        {
+
+        }
+
+    }
+}
---
+++ KafeKonsol.Data/Urun.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeKonsol.Data
+{
+    public class Urun
+    {
+        public string UrunAd { get; set; }
+        public decimal BirimFiyat { get; set; }
+        public void ToString()
+        {
+
+        }
+    }
+}
