3fad9df92825b9da713d1d98471f63f9ee76212d
Ibrahim Ozan Genç <ibrahimozan.genc@bilgeadamboost.com>
2023-05-18 11:36:33 +0300

    2.Güncelleme

--- KafeKonsol/AnaForm.Designer.cs
+++ KafeKonsol/AnaForm.Designer.cs
@@ -34,7 +34,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(806, 456);
+            ClientSize = new Size(953, 591);
             Name = "AnaForm";
             Text = "AnaForm";
             ResumeLayout(false);
